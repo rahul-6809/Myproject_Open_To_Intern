a1b4fae09b3029376fc0704eb41c3e3910b4770a
https://github.com/rahul-6809 <rkpandeyisin2015@gmail.com>
2023-06-12 22:35:32 +0530

    Done

--- src/Controllers/internController.js
+++ src/Controllers/internController.js
@@ -21,7 +21,9 @@ const createInten= async (req,res) =>{
                  const findInternMobile = await internModels.findOne({mobile: mobile})
                  if(findInternMobile) return res.status(400).send({status:false,message:"Intern already exists"})
                  else{
-                    const intern= await internModels.create(req.body)
+
+                    const internData = {name: name, email: email,mobile: mobile,collegeId: findCollege._id}
+                   const intern= await internModels.create(internData)
                     const InternResponse = {name: intern.name,   mobile: intern.mobile,    collegeid: intern.collegeId,emailId: intern.emailId,  isDelete: intern.isDelete}
                     res.status(201).send({status:true,data:InternResponse})
                  }
@@ -41,10 +43,10 @@ const GetIntenDetails= async (req, res) =>{
     try{
         const college_Name = req.params.collegeName
 
-        if(!college_Name) return res.status(404).send({status:false, message:"collegeName is required"})
+        if(!college_Name) return res.status(400).send({status:false, message:"collegeName is required"})
         else{
             const College= await collegeModels.findOne({name:college_Name})
-            if(! College) return res.status(404).send({status:false, message:" College is not found"})
+            if(! College) return res.status(400).send({status:false, message:" College is not found"})
             else{
                 const intern=await internModels.find({collegeId: College._id}.select({name:1,emailId:1,mobile:1}))
                 const collegeDetails = {name:College.name,fullName:College.fullName,logolink: College.logolink,Interns:intern}
