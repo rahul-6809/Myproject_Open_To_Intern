a26391ea4c0e04b5ea172d40a6840323af67249c
https://github.com/rahul-6809 <rkpandeyisin2015@gmail.com>
2023-06-14 22:05:58 +0530

    done final

--- src/Controllers/collegeController.js
+++ src/Controllers/collegeController.js
@@ -21,42 +21,28 @@ const createCollege = async (req, res) => {
     const { name, fullName, logoLink } = req.body;
 
     if (!isValid(name) || !isValid(fullName) || !isValid(logoLink)) {
-      return res.status(400).send({ status: false, message: "Invalid input" });
+      return res.status(400).send({ status: false, message: "Provide valid input" });
     }
 
     if (!validator.isURL(logoLink)) {
-      return res.status(400).send({
-        status: false,
-        message: "Please provide a valid logo link"
-      });
+      return res.status(400).send({status: false,  message: "Please provide a valid logo link"})
     }
     const collegeName = await collegeModel.findOne({ name });
 
-    if (collegeName) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: "college detail already exists",
-        });
+    if (collegeName) {return res.status(400) .send({ status: false, message: "college detail already exists",})
     }
 
     const collegeFullName = await collegeModel.findOne({ fullName });
 
-    if (collegeFullName) {
-      return res
-        .status(400)
-        .send({ status: false, message: "college Name already exists" });
+    if (collegeFullName) { 
+        return res.status(400).send({ status: false, message: "college Name already exists" })
     }
 
     const college = await collegeModel.create(req.body);
-
-    const requiredData = await collegeModel
-      .findById(college._id)
-      .select({ _id: 0, __v: 0 });
-
+   const requiredData = await collegeModel.findById(college._id).select({ _id: 0, __v: 0 });
     return res.status(201).send({ status: true, data: requiredData });
-  } catch (err) {
+  
+} catch (err) {
     console.log(err),
       res.status(500).send({ status: false, message: err.message });
   }
--- src/Controllers/internController.js
+++ src/Controllers/internController.js
@@ -13,45 +13,40 @@ const isValid = function(value){
 
 
 const createInten = async (req, res) => {
-  try{let { name, email, mobile, collegeName } = req.body;
+  try{
+    let { name, email, mobile, collegeName } = req.body;
 
   if (!isValid(name) ||!isValid(email) ||!isValid(mobile) ||!isValid(collegeName)) {
     return res.status(400).send({ status: false, message: "Invalid input" });
   }
 
-  
-  
-  if (!validator.isEmail(email)) {
+ if (!validator.isEmail(email)) {
     return res.status(400).send({ status: false, message: "Email should be valid email address" });
   }
  
   const intern = await internModel.findOne({ email });
-
-  if (intern) {
+ if (intern) {
     return res.status(400).send({ status: false, message: "email already exists" });
   }
 
- 
- 
-
-
-  if (!validator.isMobilePhone(mobile)) {
+if (!validator.isMobilePhone(mobile)) {
     return res.status(400).send({ status: false, message: "provide correct Mobile number" });
   }
 
-  const internExists = await internModel.findOne({ mobile });
+  const internCheck = await internModel.findOne({ mobile });
 
-  if (internExists) {
+  if (internCheck) {
     return res.status(400).send({status: false,message: "Mobile Number already present",
       });
   }
-  collegeName = collegeName.trim();
+ 
   const college = await collegeModel.findOne({ name: collegeName });
 
   if (!college) {
  return   res.status(404).send({status: false,message: " college doesn't exists",
       });
   }
+
 
     const addIntern = await internModel.create({name, email, mobile, collegeId : college._id}) 
 
@@ -71,16 +66,15 @@ try{
   let college_name = req.query.collegeName
    if(!college_name) return res.status(404).send({status : false, message:"collegeName is required"})
 
-const college = await collegeModel.findOne({name : college_name}).lean()
-if(!college)  return res.status(404).send({status:false, message:'college not found'})
-
-const intern = await internModel.find({collegeId:college._id}).select({collegeId:0, isDeleted:0,__v:0})
-delete college._id
-delete college.isDeleted
-delete college.__v
-college.interns = intern
-
-res.status(200).send({status:true, data :college})
+const college = await collegeModel.findOne({$or:[{name:college_name},{fullname:college_name}]}).lean()
+const intern= await internModel.find({collegeId:college._id,isDeleted: false})
+const details = {
+    name: college.name,
+    fullName: college.fullName,
+    logoLink: college.logoLink,
+    interns: intern
+}
+res.status(200).send({status:true, data :details})
 }catch(err){
   console.log(err) , res.status(500).send({status:false, message:err.message})
 }
